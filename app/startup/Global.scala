0eec7ed05bce3b2f9912f5bd69bb17a0435c270b
Malte Fentroß <m.fentross@app-buddy.de>
2014-09-11 13:40:00 +0200

    honestly refactored for demo now

--- app/startup/Global.scala
+++ app/startup/Global.scala
@@ -21,7 +21,7 @@ object Global extends GlobalSettings{
 //    println(request.domain)
 //    val add = request.domain
     action.apply(request).map(_.withHeaders(
-      "Access-Control-Allow-Origin" -> "http://office.app-buddy.de",
+      "Access-Control-Allow-Origin" -> "http://demo.flimmr.io",
 //      "Access-Control-Allow-Origin" -> "http://onair.herokuapp.com",
       "Access-Control-Allow-Methods" -> "POST, GET, PUT, DELETE, OPTIONS",
       "Access-Control-Allow-Credentials" -> "true"
